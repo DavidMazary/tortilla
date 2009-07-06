8dd39228c3e6ccf1e299778991a61289dc18de75
David Mazary <DavidMazary@users.noreply.github.com>
2009-07-06 19:05:57 -0400

    Close tortilla after connecting to server to free up memory

--- Tortilla/src/tortilla/GameLauncher.java
+++ Tortilla/src/tortilla/GameLauncher.java
@@ -67,6 +67,8 @@ public class GameLauncher {
             try {
                 Runtime runtime = Runtime.getRuntime();
                 runtime.exec(cmd);
+                Thread.sleep(5000);
+                System.exit(0);
             } catch (Exception ex) {
                 Logger.getLogger(GameLauncher.class.getName()).log(
                         Level.SEVERE, null, ex);
