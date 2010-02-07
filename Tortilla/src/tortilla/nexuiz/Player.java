47747c2948788fa7f24b6ac8c016226b764206ad
dmazary <dmazary@gmail.com>
2010-02-07 00:56:50 -0500

    Font char lookup table is static.

--- Tortilla/src/tortilla/nexuiz/Player.java
+++ Tortilla/src/tortilla/nexuiz/Player.java
@@ -31,45 +31,10 @@ public class Player {
         try {
             byte nameBytes[] = name.getBytes("ISO-8859-1");
             StringBuilder sb = new StringBuilder("");
-            // Thanks to KadaverJack for showing me this lovely bit here :)
-            char fontTable[] = {
-                ' ', '#', '#', '#', '#', '.', '#', '#',
-                '#', 9, 10, '#', ' ', 13, '.', '.',
-                '[', ']', '0', '1', '2', '3', '4', '5',
-                '6', '7', '8', '9', '.', '<', '=', '>',
-                ' ', '!', '"', '#', '$', '%', '&', '\'',
-                '(', ')', '*', '+', ',', '-', '.', '/',
-                '0', '1', '2', '3', '4', '5', '6', '7',
-                '8', '9', ':', ';', '<', '=', '>', '?',
-                '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G',
-                'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
-                'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
-                'X', 'Y', 'Z', '[', '\\', ']', '^', '_',
-                '`', 'a', 'b', 'c', 'd', 'e', 'f', 'g',
-                'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
-                'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
-                'x', 'y', 'z', '{', '|', '}', '~', '<',
-                '<', '=', '>', '#', '#', '.', '#', '#',
-                '#', '#', ' ', '#', ' ', '>', '.', '.',
-                '[', ']', '0', '1', '2', '3', '4', '5',
-                '6', '7', '8', '9', '.', '<', '=', '>',
-                ' ', '!', '"', '#', '$', '%', '&', '\'',
-                '(', ')', '*', '+', ',', '-', '.', '/',
-                '0', '1', '2', '3', '4', '5', '6', '7',
-                '8', '9', ':', ';', '<', '=', '>', '?',
-                '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G',
-                'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
-                'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
-                'X', 'Y', 'Z', '[', '\\', ']', '^', '_',
-                '`', 'a', 'b', 'c', 'd', 'e', 'f', 'g',
-                'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
-                'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
-                'x', 'y', 'z', '{', '|', '}', '~', '<'
-            };
-
+            
             // Character will correspond with unsigned byte.
             for (byte b : nameBytes) {
-                sb.append(fontTable[b & 0xff]);
+                sb.append(PlayerUtils.fontTable[b & 0xff]);
             }
             asciiName = sb.toString();
         } catch (UnsupportedEncodingException e) {
@@ -170,7 +135,7 @@ public class Player {
         String htmlName = newName;
         boolean firstTag = true;
         for (int i = 0; i < htmlName.length(); i++) {
-            if ((htmlName.charAt(i)) == '^' && (i != htmlName.length())) {
+            if ((htmlName.charAt(i)) == '^' && (i < htmlName.length() - 1)) {
                 char nextChar = htmlName.charAt(i + 1);
                 String color;
                 if (Character.isDigit(nextChar)) {
